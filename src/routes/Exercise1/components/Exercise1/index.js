42a9c59f3926c8d60ea741adb14820da7eed2b5c
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2017-10-30 20:33:15 +0100

    optimized code

--- src/routes/Exercise1/components/Exercise1/index.js
+++ src/routes/Exercise1/components/Exercise1/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { MainButton, Brick, Yellow, Move } from './styles'
 
-let count = 0;
 
 
 export default class Exercise1 extends Component {
@@ -11,46 +10,29 @@ export default class Exercise1 extends Component {
     super(props)
     this.state = {
       textContent : 'Clicca e fai cambiare colore dello sfondo',
-      secondClick : false,
-      thirdClick : false,
-      fourthClick : false
+      count : 0
     }
     this.handleClick = this.handleClick.bind(this);   
   }
 
   handleClick() {
-    switch(count){
-      case 0 : {
-        document.body.style.background = "lightblue";
-        this.setState ({textContent : 'Clicca per creare un mattoncino'});
-        count ++;
-        } 
-        break;
-      case 1: {
-        this.setState ({secondClick : true});
-        count ++;
-        }
-        break;
-      case 2 : {
-        this.setState ({thirdClick : true});
-        count ++;
-      }
-      break;
-      case 3 : {
-        this.setState ({thirdClick : false});
-        this.setState ({fourthClick : true});
-      }
-    }
+    this.setState({
+      count : ++this.state.count 
+    })
+    console.log(this.state.count)
   }
   
   render () {
-    
-    return (
+    const { count, textContent } = this.state
+    const secondClick = count > 0
+    const thirdClick = count > 1
+    const fourthClick = count > 2
+  return (
     <div>    
-    {!this.state.secondClick ? (<MainButton onClick={this.handleClick}>{this.state.textContent}</MainButton>) : 
+    {!secondClick ? (<MainButton onClick={this.handleClick}>{textContent}</MainButton>) : 
     (<div><p>Passa col mouse sul mattoncino per farlo ruotare, cliccaci sopra per crearne un altro</p><Brick onClick={this.handleClick}>M</Brick></div>)} 
-    {(this.state.secondClick && this.state.thirdClick) ? (<div><p>Questo invece si espande, clicca anche su questo</p><Yellow onClick={this.handleClick}></Yellow></div>) : (<div></div>)}
-    {(!this.state.thirdClick && this.state.fourthClick) ? (<div><p>Questo invece si muove</p><Move></Move></div>) : (<div></div>)}
+    {(secondClick && thirdClick) ? (<div><p>Questo invece si espande, clicca anche su questo</p><Yellow onClick={this.handleClick}></Yellow></div>) : (<div></div>)}
+    {(!thirdClick && fourthClick) ? (<div><p>Questo invece si muove</p><Move></Move></div>) : (<div></div>)}
       </div>
     );
   }
